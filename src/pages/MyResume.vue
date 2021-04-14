ff0144a997fc110e26115c7efdda3188cc4779da
DD <dd.vs@hotmail.com>
2021-04-14 17:20:12 +0530

    added resume

---
+++ src/pages/MyResume.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="canvas">
+    <div class="page">
+      <h1>My resume</h1>
+    </div>
+  </div>
+  <teleport to="body">
+    <div class="mask"></div>
+  </teleport>
+</template>
+
+<style scoped>
+.mask {
+  width: 100%;
+  margin: 0;
+  z-index: 10;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  background-color: rgb(105, 99, 115);
+}
+.canvas {
+  background-color: rgb(122, 122, 122);
+}
+.page {
+  width: 900px;
+  height: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 25px;
+  margin-bottom: 25px;
+  background-color: #fff;
+  position: relative;
+  z-index: 11;
+  box-shadow: 5px 5px 20px #0e0e0e;
+  padding: 50px 35px;
+  overflow: hidden;
+}
+body {
+  background-color: black;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import TheHome from "./pages/TheHome.vue";
 import TheComponents from "./pages/TheComponents.vue";
+import MyResume from "./pages/MyResume.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   mode: "history",
   routes: [
     { path: "/", redirect: { name: "Home" } },
+    { path: "/resume", name: "Resume", component: MyResume },
     { path: "/home", name: "Home", component: TheHome },
     { path: "/components", name: "Components", component: TheComponents },
   ],
